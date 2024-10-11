4766328c32dc437b91a32fde56b6c6ba18320179
Ben Meier <ben@meierhost.com>
2024-10-11 19:40:16 +0100

    chore: remove cobra

--- go.mod
+++ go.mod
@@ -11,10 +11,7 @@ require (
 	github.com/onsi/ginkgo/v2 v2.7.0
 	github.com/onsi/gomega v1.24.2
 	github.com/pkg/errors v0.9.1
-	github.com/pkg/profile v1.6.0
 	github.com/sirupsen/logrus v1.7.0
-	github.com/spf13/cobra v1.1.1
-	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.8.0
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -25,13 +22,14 @@ require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
--- testsupport/log_init.go
+++ testsupport/log_init.go
@@ -1,18 +1,12 @@
 package testsupport
 
 import (
-	"flag"
-	"os"
-
 	"github.com/davecgh/go-spew/spew"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/pflag"
 )
 
 func init() {
-	lvl := parseLogLevel()
-	log.SetLevel(lvl)
-	log.Warnf("Running test with logs in '%s' level", lvl.String())
+	log.SetLevel(log.InfoLevel)
 	log.SetFormatter(&log.TextFormatter{
 		DisableQuote: true, // see https://github.com/sirupsen/logrus/issues/608#issuecomment-745137306
 	})
@@ -22,22 +16,4 @@ func init() {
 	spew.Config.DisablePointerAddresses = true
 	spew.Config.DisablePointerMethods = true
 	spew.Config.DisableUnexported = true
-}
-
-func parseLogLevel() log.Level {
-	var logLevel string
-	// needed to let ginkgo parse the flag, otherwise, `ginkgo -- --loglevel=...` will fail with `flag provided but not defined: -loglevel`
-	flag.StringVar(&logLevel, "loglevel", "error", "log level to set [debug|info|warn|error|fatal|panic]")
-	// parse with a custom flagset in which all other flags (ginkgo's) are ignored
-	f := pflag.NewFlagSet("passthroughs", pflag.ContinueOnError)
-	f.ParseErrorsWhitelist.UnknownFlags = true
-	f.StringVarP(&logLevel, "loglevel", "l", "error", "log level to set [debug|info|warn|error|fatal|panic]")
-	if err := f.Parse(os.Args[1:]); err != nil {
-		panic(err)
-	}
-	lvl, err := log.ParseLevel(logLevel)
-	if err != nil {
-		panic(err)
-	}
-	return lvl
 }
